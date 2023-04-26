63fc1dc17b7ccf035418892102dc04a17de387b0
Antonio Tan <44644245+AntonioTan@users.noreply.github.com>
2023-04-25 22:49:16 -0400

    First Version

---
+++ src/main/java/com/stori/toyfinsys/ToyFinSysApplication.java
@@ -0,0 +1,13 @@
+package com.stori.toyfinsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyFinSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyFinSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stori/toyfinsys/ToyFinSysApplicationTests.java
@@ -0,0 +1,12 @@
+package com.stori.toyfinsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyFinSysApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
